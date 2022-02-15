3d7a3c8e144f00ed1498384c29fc48fb4f074392
AyaJas <aya.hameedat@gmail.com>
2022-02-16 00:32:05 +0200

    class-18

---
+++ src/components/ModalMovie/ModalMovie.js
@@ -0,0 +1,31 @@
+import Modal from 'react-bootstrap/Modal';
+import { Button } from 'react-bootstrap';
+
+
+function ModalMovie({ movieInfo, Add, handleAdd }) {
+
+return (
+    <>
+    <Modal show={Add} onHide={handleAdd} animation={false}>
+        <Modal.Header closeButton>
+            <Modal.Title>Add It To Favorite</Modal.Title>
+        </Modal.Header>
+        <Modal.Body className="body">
+            <h3>{movieInfo.title}</h3>
+            <img variant="top" src={`https://www.themoviedb.org/t/p/w220_and_h330_face${movieInfo.poster_path}`} />
+            <div>
+                <label htmlFor="op">Write Your Opinion</label>
+                <input placeholder="Write Your Opinion" type="text" id="op" />
+            </div>
+        </Modal.Body>
+        <Modal.Footer>
+            <Button variant="primary"> Add To Favorite </Button>
+        </Modal.Footer>
+    </Modal>
+</>
+
+);
+
+}
+
+export default ModalMovie;
